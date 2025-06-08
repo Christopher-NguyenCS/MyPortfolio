5b7b074b86d219e80fc6ac89a5e58dd98a31648f
Christopher Nguyen <christophernng1999@gmail.com>
2025-06-08 16:17:38 -0400

    fix: Fix styles conflicting with Header
    I have move NavigationTab into Header, so now I have to fix
    NavigationTab's styles because this component is dependent with Header's
    styles.

--- src/components/NavigationTab.tsx
+++ src/components/NavigationTab.tsx
@@ -1,31 +1,22 @@
 // src/components/NavigationTab.jsx
-
-import { Outlet, Link } from 'react-router';
+import { Link } from "react-router";
 
 export default function NavigationTab() {
 	return (
-		<div className="flex flex-col bg-gray-50 min-w-full">
-			<nav className="bg-gray-800 text-white p-4 shadow-lg flex min-w-full">
-				<Link
-					to="/"
-					className="block py-2 px-3 rounded-md text-sm font-medium hover:bg-gray-700 transition-colors duration-200 mr-4 md:mr-0 md:mb-2"
-				>
-					Home
-				</Link>
-				<Link
-					to="/projects"
-					className="block py-2 px-3 rounded-md text-sm font-medium hover:bg-gray-700 transition-colors duration-200"
-				>
-					Projects
-				</Link>
-			</nav>
-
-			<main className="flex-grow p-6 bg-white overflow-y-auto">
-				<Outlet />
-			</main>
-
-
-		</div>
+		<nav className="bg-gray-800 text-white p-5 shadow-lg flex ">
+			<Link
+				to="/"
+				className="m-5"
+			>
+				Home
+			</Link>
+			<Link
+				to="/projects"
+				className="m-5"
+			>
+				Projects
+			</Link>
+		</nav>
 	);
 }
 
