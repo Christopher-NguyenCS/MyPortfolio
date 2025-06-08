60c7b68171310caa085126d0906327b104a5ae5a
Christopher Nguyen <christophernng1999@gmail.com>
2025-06-08 16:14:33 -0400

    fix: Remove misplaced components
    Since I changed the navigation and routes in main.tsx, I have to remove
    components conflicting with App.tsx component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,14 @@
-import { useState } from 'react';
-import { Button } from "@/components/ui/button";
-import './App.css';
-import NavigationTab from './components/NavigationTab';
+
+
+
 function App() {
 
 
 	return (
 		<>
-			<div className='flex sm:flex-col md:flex-row'>
-				<header>
-					<h1 className='p-5'>
-						Christopher Nguyen
-					</h1>
-				</header>
-				<NavigationTab />
+			<div>
+				Hello, my name is Christopher Nguyen and I am an aspiring full stack developer!
 			</div>
-			<div className="flex flex-col justify-center items-center">
-				<header>
-					<h1>
-						Hi this is Christopher Nguyen. I am an aspiring software developer focus in full stack development!
-					</h1>
-				</header>
-			</div>
-
 		</>
 	)
 }
