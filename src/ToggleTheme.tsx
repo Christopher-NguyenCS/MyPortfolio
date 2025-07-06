c0b8c40fdd9f277f3133864e879c7632556612bc
Christopher Nguyen <christophernng1999@gmail.com>
2025-07-06 10:21:56 -0400

    style: Update style with Animal.tsx
    Adjust the style for adding Animal component to my portfolio.

--- src/ToggleTheme.tsx
+++ src/ToggleTheme.tsx
@@ -2,7 +2,7 @@ import { ModeToggle } from './components/mode-toggle.tsx'
 
 export default function ToggleTheme() {
 	return (
-		<div className='flex justify-center items-start py-2'>
+		<div className='flex justify-center items-start py-5'>
 			<ModeToggle />
 		</div>
 	)
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,8 @@ export default function Header() {
 
 	return (
 		<>
-			<header className="flex justify-center space-x-4 md:flex-row dark:text-white w-full p-5">
+			<header className="flex justify-center items-center space-x-4 md:flex-row dark:text-white w-full p-5">
+
 				<h1 className="dark:text-white py-5">
 					Christopher Nguyen
 				</h1>
